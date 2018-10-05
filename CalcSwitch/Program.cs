fb5cb3f078962e2f1d7dcb71d86ab987351181a2
ARTICUS939 <articus_mib@mail.ru>
2018-10-05 17:41:38 +0300

    Кальк со switch

--- CalcSwitch/Program.cs
+++ CalcSwitch/Program.cs
@@ -10,6 +10,60 @@ namespace CalcSwitch
     {
         static void Main(string[] args)
         {
+            double a;
+            double b;
+            double total = 0;
+            char oper;
+            bool ok = true;
+
+
+            Console.WriteLine("Введите первое число:");
+            a = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Введите действие:");
+            oper = Convert.ToChar(Console.ReadLine());
+
+            Console.WriteLine("Введите второе число:");
+            b = Convert.ToDouble(Console.ReadLine());
+
+            
+            switch (oper)
+            {
+                case '+':
+                    total = a + b;
+                    break;
+                case '-':
+                    total = a - b;
+                    break;
+                case '*':
+                    total = a * b;
+                    break;
+                case '/':
+                    if (b != 0)      //во избежания деления на ноль
+                    {
+                        total = a / b;
+                        break;
+                    }
+                    else
+                    {
+                        goto default;
+                    }
+                default:
+                    ok = false;
+                    break;
+            }
+            if (ok)
+            {
+                Console.WriteLine();
+                Console.WriteLine();
+                Console.WriteLine("Ответ: " + total);
+            }
+            else
+            {
+                Console.WriteLine("error");
+            }
+
+            Console.ReadLine();
         }
     }
 }
